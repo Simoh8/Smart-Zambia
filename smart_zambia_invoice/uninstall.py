906df9123ade91646f0b5de2ed0bb1f1d3a7d02e
simoh8 <simomutu8@gmail.com>
2024-11-14 18:34:00 +0300

    deleting the custom fields update

--- smart_zambia_invoice/uninstall.py
+++ smart_zambia_invoice/uninstall.py
@@ -2,7 +2,7 @@ import frappe
 
 def cleanup_custom_fields():
 
-    custom_fields = frappe.get_all("Custom Field", filter={"dt":"Item"}, fields=["name"])
+    custom_fields = frappe.get_all("Custom Field", filters={"dt":"Item"}, fields=["name"])
 
     for field in custom_fields:
         frappe.delete_doc("Custom Field", field["name"], ignore_missing=True)
