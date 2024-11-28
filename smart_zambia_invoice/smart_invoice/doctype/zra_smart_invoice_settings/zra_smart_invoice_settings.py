912deedf858b5d47ece4b7f76b96d4f601e354ae
simoh8 <simomutu8@gmail.com>
2024-11-28 15:20:15 +0300

    updates on config

--- smart_zambia_invoice/smart_invoice/doctype/zra_smart_invoice_settings/zra_smart_invoice_settings.py
+++ smart_zambia_invoice/smart_invoice/doctype/zra_smart_invoice_settings/zra_smart_invoice_settings.py
@@ -1,7 +1,11 @@
 # Copyright (c) 2024, simon muturi and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import _asyncio
+import aiohttp
+import frappe.defaults
+from frappe.integrations.utils import create_request_log
 from frappe.model.document import Document
 
 
