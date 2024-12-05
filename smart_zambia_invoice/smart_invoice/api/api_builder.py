6bd25b51d4444be523236263b6706fa0949ff3d1
simoh8 <simomutu8@gmail.com>
2024-12-05 15:14:01 +0300

    updates on api builder

--- smart_zambia_invoice/smart_invoice/api/api_builder.py
+++ smart_zambia_invoice/smart_invoice/api/api_builder.py
@@ -9,6 +9,7 @@ import aiohttp
 
 import frappe
 from frappe.model.document import Document
+from ..utilities import make_post_request
 
 
 class BaseEndpointConstructor:
@@ -143,3 +144,37 @@ def perform_remote_calls(self, doctype: Document |str |None =None, document_name
         title= "Critical Setup Error",
         is_minimizable=True
      )
+
+    self.doctype, self.document_name= doctype, document_name
+    parsed_url =parse.urlparse(self._url)
+    route_path = f"/{parsed_url.path.split('/')[-1]}"
+
+    self.interation_request = create_request_log(
+     data=self._payload,
+     is_remote_request= True,
+     service_name="vscd"
+     request_headers=self._headers,
+     url= self._url,
+     reference_docname=document_name,
+     reference_doctype=doctype,
+    )
+
+    try:
+        response =_asyncio.run( make_post_request(self._url, self._payload, self._headers))
+
+        if response["resultCd"]== "000":
+            
+            self._success_callback_handler(response)
+
+            update_last_request_dates(response["resultDt"], route_path)
+
+            update_intergration_request(self.intergration_request_name.name, status= "Completed" ,output=response["resultMsg"], error=None,)
+
+        else:
+            update_intergration_request(
+                self.intergration_request.name,
+                
+            )
+
+
+
