755ec9d52bfd1989d1ce0b16b5279c8a840c4630
Simoh8 <Simomutu8@gmail.com>
2025-03-18 10:00:48 +0300

    updates on the background jobs for updating the codes and countries

--- smart_zambia_invoice/smart_invoice/background_jobs/jobs.py
+++ smart_zambia_invoice/smart_invoice/background_jobs/jobs.py
@@ -3,10 +3,14 @@ import json
 import frappe
 import frappe.defaults
 from frappe.model.document import Document
+from smart_zambia_invoice.smart_invoice.overrides.backend.common_overrides import EndpointConstructor, build_request_headers, get_route_path, get_server_url, on_error
 from..overrides.backend.stock_ledger_entry import on_update
 from..overrides.backend.sales_invoice import on_submit
 
 from ..api.zra_api import save_stock_inventory,perform_zra_notice_search
+
+
+endpoint_maker = EndpointConstructor()
 
 
 def frequent_refresh_notices() -> None:
@@ -92,3 +96,220 @@ def submit_pos_invoices_information() -> None:
                 ) 
             except TypeError:
                 continue
+
+
+
+
+
+
+@frappe.whitelist()
+def refresh_code_lists(vendor: str="OSCU KRA") -> str | None:
+    company_name: str | None = frappe.defaults.get_user_default("Company")
+
+    headers = build_request_headers (company_name, vendor)
+    server_url = get_server_url(company_name, vendor)
+
+    code_search_route_path, last_request_date = get_route_path(
+        "CodeSearchReq"
+    )  # endpoint for code search
+
+    if headers and server_url and code_search_route_path:
+        url = f"{server_url}{code_search_route_path}"
+        payload = {
+            "lastReqDt": "20200101000000"
+        }  # Hard-coded to this date to get all code lists.
+
+        endpoint_maker.headers = headers
+        endpoint_maker.payload = payload
+        endpoint_maker.error_callback = on_error
+
+        # Fetch and update codes obtained from CodeSearchReq endpoint
+        endpoint_maker.url = url
+        endpoint_maker.success_callback = run_updater_functions
+        endpoint_maker.make_remote_call(doctype=None, document_name=None)
+
+        return "succeeded"
+
+
+@frappe.whitelist()
+def get_item_classification_codes() -> str | None:
+    company_name: str | None = frappe.defaults.get_user_default("Company")
+
+    headers = build_request_headers(company_name)
+    server_url = get_server_url(company_name)
+
+    item_cls_route_path, last_request_date = get_route_path(
+        "ItemClsSearchReq"
+    )  # overwriting last_request_date since it's not used elsewhere for this task
+
+    if headers and server_url and item_cls_route_path:
+        url = f"{server_url}{item_cls_route_path}"
+        payload = {
+            "lastReqDt": "20230101000000"
+        }  # Hard-coded to a this date to get all code lists.
+
+        endpoint_maker.url = url
+        endpoint_maker.headers = headers
+        endpoint_maker.payload = payload
+        endpoint_maker.error_callback = on_error
+
+        # Fetch and update item classification codes from ItemClsSearchReq endpoint
+        endpoint_maker.url = f"{server_url}{item_cls_route_path}"
+        endpoint_maker.success_callback = update_item_classification_codes
+
+        # endpoints_builder.make_remote_call(doctype=None, document_name=None)
+        frappe.enqueue(
+            endpoint_maker.perform_remote_calls,
+            is_async=True,
+            queue="long",
+            timeout=1200,
+            doctype="ZRA Smart Invoice Settings",
+        )
+
+        return "succeeded"
+
+
+def run_updater_functions(response: dict) -> None:
+    for class_list in response["data"]["clsList"]:
+        if class_list["cdClsNm"] == "Quantity Unit":
+            update_unit_of_quantity(class_list)
+
+        if class_list["cdClsNm"] == "Taxation Type":
+            update_taxation_type(class_list)
+
+        if class_list["cdClsNm"] == "Packing Unit":
+            update_packaging_units(class_list)
+
+        if class_list["cdClsNm"] == "Country":
+            update_countries(class_list)
+
+
+def update_unit_of_quantity(data: dict) -> None:
+    doc: Document | None = None
+
+    for unit_of_quantity in data["dtlList"]:
+        try:
+            doc = frappe.get_doc("ZRA Unit of Quantity", unit_of_quantity["cd"])
+
+        except frappe.DoesNotExistError:
+            doc = frappe.new_doc("ZRA Unit of Quantity")
+
+        finally:
+            doc.code = unit_of_quantity["cd"]
+            doc.sort_order = unit_of_quantity["srtOrd"]
+            doc.code_name = unit_of_quantity["cdNm"]
+            doc.code_description = unit_of_quantity["cdDesc"]
+
+            doc.save()
+
+    frappe.db.commit()
+
+
+def update_taxation_type(data: dict) -> None:
+    doc: Document | None = None
+
+    for taxation_type in data["dtlList"]:
+        try:
+            doc = frappe.get_doc("ZRA Tax Type", taxation_type["cd"])
+
+        except frappe.DoesNotExistError:
+            doc = frappe.new_doc("ZRA Tax Type")
+
+        finally:
+            doc.cd = taxation_type["cd"]
+            doc.cdnm = taxation_type["cdNm"]
+            doc.cddesc = taxation_type["cdDesc"]
+            doc.useyn = 1 if taxation_type["useYn"] == "Y" else 0
+            doc.srtord = taxation_type["srtOrd"]
+            doc.userdfncd1 = taxation_type["userDfnCd1"]
+            doc.userdfncd2 = taxation_type["userDfnCd2"]
+            doc.userdfncd3 = taxation_type["userDfnCd3"]
+
+            doc.save()
+
+    frappe.db.commit()
+
+
+def update_packaging_units(data: dict) -> None:
+    doc: Document | None = None
+
+    for packaging_unit in data["dtlList"]:
+        try:
+            doc = frappe.get_doc("ZRA Packaging Unit", packaging_unit["cd"])
+
+        except frappe.DoesNotExistError:
+            doc = frappe.new_doc("ZRA Packaging Unit")
+
+        finally:
+            doc.code = packaging_unit["cd"]
+            doc.code_name = packaging_unit["cdNm"]
+            doc.sort_order = packaging_unit["srtOrd"]
+            doc.code_description = packaging_unit["cdDesc"]
+
+            doc.save()
+
+    frappe.db.commit()
+
+
+def update_countries(data: dict) -> None:
+    doc: Document | None = None
+
+    for country in data["dtlList"]:
+        try:
+            doc = frappe.get_doc("Smart Zambia Country", country["cdNm"])
+
+        except frappe.DoesNotExistError:
+            doc = frappe.new_doc("Smart Zambia Country")
+
+        finally:
+            doc.code = country["cd"]
+            doc.code_name = country["cdNm"]
+            doc.sort_order = country["srtOrd"]
+            doc.code_description = country["cdDesc"]
+
+            doc.save()
+
+    frappe.db.commit()
+
+
+def update_item_classification_codes(response: dict) -> None:
+    code_list = response["data"]["itemClsList"]
+    existing_classifications = {
+        classification["name"]: classification
+        for classification in frappe.get_all("ZRA Item Classification", ["*"])
+    }
+
+    for item_classification in code_list:
+        if item_classification["itemClsCd"] in existing_classifications:
+            update_query = f"""
+                UPDATE `tab{"ZRA Item Classification"}`
+                SET itemclscd = '{item_classification["itemClsCd"]}',
+                    itemclslvl = '{item_classification["itemClsLvl"]}',
+                    itemclsnm = '{item_classification["itemClsNm"].replace("'", " ")}',
+                    taxtycd = '{item_classification["taxTyCd"]}',
+                    useyn = '{1 if item_classification["useYn"] == "Y" else 0}',
+                    mjrtgyn  = '{1 if item_classification["mjrTgYn"] == "Y" else 0}',
+                    modified = SYSDATE()
+                WHERE name = '{item_classification["itemClsCd"]}';
+            """
+
+            frappe.db.sql(update_query)
+
+        else:
+            insert_query = f"""
+                INSERT INTO `tab{"ZRA Item Classification"}`
+                    (name, itemclscd, itemclslvl, itemclsnm, taxtycd, useyn, mjrtgyn, creation)
+                VALUES
+                    ('{item_classification["itemClsCd"]}',
+                     '{item_classification["itemClsCd"]}',
+                     '{item_classification["itemClsLvl"]}',
+                     '{item_classification["itemClsNm"].replace("'", " ")}',
+                     '{item_classification["taxTyCd"]}',
+                     '{1 if item_classification["useYn"] == "Y" else 0}',
+                     '{1 if item_classification["mjrTgYn"] == "Y" else 0}',
+                     SYSDATE());
+            """
+
+            frappe.db.sql(insert_query)
+
+    frappe.db.commit()
